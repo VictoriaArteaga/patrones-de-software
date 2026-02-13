0d559c5881c3f4ec5410a39e0761d48459bf0799
María Victoria <maria.arteagare@campusucc.edu.co>
2026-02-13 15:54:20 -0500

    Taller 1: Patron Singleton

--- Clase 13 - 02/src/Animals.java
+++ Clase 13 - 02/src/Animals.java
@@ -1,15 +1,15 @@
 public class Animals {
-    private static Animals instance;
+    private static Animals animal;
 
     private Animals() {
         System.out.println("Create of instance");
     }
 
     public static Animals getAnimal() {
-        if (instance == null) {
-            instance = new Animals();
+        if (animal == null) {
+            animal = new Animals();
         }
-        return instance;
+        return animal;
     }
 
 
